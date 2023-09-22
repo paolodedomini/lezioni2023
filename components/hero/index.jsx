8d972734bd65decd4a39556a655c74ff39dabf91
grafica <paolo@monema.it>
2023-09-23 00:27:19 +0200

    feature: inserito box info fixed

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -1,7 +1,7 @@
 import style from './style.module.scss'
 import Image from 'next/image'
-import { useState, useRef } from 'react'
-
+import { useState, useRef, useEffect } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 const data = [
     {
         name: 'Fermata 125',
@@ -31,33 +31,62 @@ const data = [
 function Hero() {
     const [active, setActive] = useState(-1)
     const refContent = useRef(null)
-
+    const [pos, setPos] = useState(0)
     //se l'elemento è attivo carica la stessa immagine sul box informazioni nella posizione corretta e fa partire l'animaizone
-    if (refContent.current != null) {
-        if (active !== -1) {
-            console.log(refContent.current.children[active])
-        }
+    useEffect(() => {
 
-    }
+        if (refContent.current != null) {
+            if (active !== -1) {
 
-    return (<div className={style.heroContainer} ref={refContent}>
-        {data.map((item, index) => {
-            return (
-
-                <div onClick={() => { setActive(index) }} className={`style.heroContainer__heroBox ${active === index && style["heroContainer__heroBox--active"]}`} key={index}>
-                    <div className={style.heroContainer__heroBox__image} >
-                        <Image src={item.img} alt={item.name} layout="fill" objectFit="cover" />
-
-                    </div>
-                </div>
-
-
-            )
-        })
+                setPos(refContent.current.children[active].offsetLeft)
+            }
 
         }
 
-    </div>)
+    }, [pos])
+
+    console.log(pos)
+    return (
+        <>
+            <AnimatePresence>
+                {(active != -1) &&
+                    <motion.div className={style.heroInfo}
+                        initial={{ opacity: 0, width: 0 }}
+                        animate={{ opacity: 1, width: '100%' }}
+                        exit={{ opacity: 0, width: 0 }}
+                        transition={{ duration: .5, ease: 'easeInOut' }}
+                    >  <div onClick={() => setActive(-1)}>close</div>
+                        <motion.div className={style.heroInfo__boxImage}
+                            initial={{ opacity: 0, left: pos }}
+                            animate={{ opacity: 1, left: 0 }}
+                            exit={{ opacity: 0, left: '50%' }}
+                            transition={{ delay: .5, duration: .5, ease: 'easeInOut' }}
+                        >
+                            <Image src={data[active]?.img} alt={data[active]?.name} width={100} height={400} />
+                        </motion.div>
+
+                    </motion.div>}
+            </AnimatePresence>
+            <div className={style.heroContainer} ref={refContent}>
+
+                {
+                    data.map((item, index) => {
+                        return (
+
+                            <div onClick={() => { setActive(index) }} className={`style.heroContainer__heroBox`} key={index}>
+                                <div className={style.heroContainer__heroBox__image} >
+                                    <Image src={item.img} alt={item.name} layout="fill" objectFit="cover" />
+
+                                </div>
+                            </div>
+
+
+                        )
+                    })
+
+                }
+
+            </div ></>)
 }
 
 export default Hero
