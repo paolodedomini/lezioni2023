921b6c332f967fe8ec0fe66874548fcb5b58456e
grafica <paolo@monema.it>
2023-09-22 10:13:57 +0200

    feature: logica apertura menu principale

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import style from './style.module.scss';
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { useState } from 'react';
 
 const data = [
     { name: 'prodotti', path: '/prodotti' },
@@ -8,12 +10,14 @@ const data = [
 ]
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <>
-
-            <nav>
-
-                <ul class={style.mainNav}>
+            <nav className={style.navBar}>
+                <div onClick={() => { setMenuOpen(!menuOpen) }} className={style.navBar__menuIcon}>
+                    <RxHamburgerMenu /></div>
+                <ul className={`${style.navBar__mainNav} ${menuOpen && style['navBar__mainNav--open']}`}>
                     {data.map((item, index) => (
                         <li key={index}>
                             <Link href={item.path}>{item.name}</Link>
