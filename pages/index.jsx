6e5481ef54aa9c49b09c40ef4cd8b8f2578cd873
grafica <paolo@monema.it>
2023-09-25 12:41:38 +0200

    struttura divisorio

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Hero from '../components/hero'
-
+import Divisorio from '../components/divisorio'
 export default function Home() {
   return (
     <>
       <Hero />
+      <Divisorio tooltip={'test'} />
     </>
   )
 }
