e0966e145a9f0c91830aea1b28d8ec01250205ce
grafica <paolo@monema.it>
2023-09-25 10:02:43 +0200

    feature: animazioni e stile generale

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -5,27 +5,31 @@ import { AnimatePresence, motion } from 'framer-motion'
 const data = [
     {
         name: 'Fermata 125',
+        cantina: 'Cantina di Radda',
         path: '/prodotti/chianti-classico',
         img: '/immagini-prodotto/fermata125.png',
         text: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.'
     },
     {
         name: 'Ferrero',
+        cantina: 'Cantina di Radda',
         path: '/prodotti/chianti-classico',
         img: '/immagini-prodotto/ferrero.png',
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.'
+        text: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.'
     },
     {
         name: 'Syrah',
+        cantina: 'Cantina di Radda',
         path: '/prodotti/chianti-classico',
         img: '/immagini-prodotto/syrah.png',
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.'
+        text: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.'
     },
     {
         name: 'Valpolicella',
+        cantina: 'Cantina di Radda',
         path: '/prodotti/chianti-classico',
         img: '/immagini-prodotto/valpolicella.png',
-        text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.'
+        text: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione.'
     }
 ]
 function Hero() {
@@ -77,7 +81,7 @@ function Hero() {
                                 }
                             }}
                             exit={{
-                                opacity: 0, left: pos.x, top: pos.y,
+                                opacity: 0,
                                 transition: {
                                     duration: 1,
                                     ease: 'easeInOut',
@@ -94,16 +98,17 @@ function Hero() {
                                 }}
                                 exit={{ opacity: 0 }}
                             >
-                                <motion.h2
+                                <motion.header
                                     initial={{ opacity: 0, top: 10 }}
                                     animate={{
                                         opacity: 1, top: 0,
                                         transition: { delay: 3, duration: 1 }
                                     }}
                                     exit={{ opacity: 0 }}
-                                >{data[active]?.name}
-                                </motion.h2>
-
+                                >
+                                    <h2>{data[active]?.name}</h2>
+                                    <h3>{data[active].cantina}</h3>
+                                </motion.header>
                                 <motion.p
                                     initial={{ opacity: 0, top: 10 }}
                                     animate={{
@@ -112,7 +117,9 @@ function Hero() {
                                     }}
                                     exit={{ opacity: 0 }}
 
-                                > <hr />{data[active]?.text}</motion.p>
+                                > <hr />{data[active]?.text}
+                                    <Image src="/ui/freccia.svg" alt="arrow" width={400} height={30} />
+                                </motion.p>
                             </motion.div>
                         </motion.div>
 
