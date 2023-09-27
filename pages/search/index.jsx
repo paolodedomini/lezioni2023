9d70e6ce176c8b0d5150ca82f607d42126e27427
grafica <paolo@monema.it>
2023-09-27 17:36:33 +0200

    creazione pagina di ricerca

---
+++ pages/search/index.jsx
@@ -0,0 +1,10 @@
+function searchPage() {
+    return (
+        <section className="section1">
+
+            <h1>Risultati di ricerca</h1>
+
+        </section>
+    )
+}
+export default searchPage
