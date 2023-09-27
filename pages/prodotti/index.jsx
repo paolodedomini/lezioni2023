9839672b2d2e7ae2160fec33043bb157bf606355
grafica <paolo@monema.it>
2023-09-27 10:03:25 +0200

    pagina prodotti

---
+++ pages/prodotti/index.jsx
@@ -0,0 +1,66 @@
+import GrigliaProdotti from "../../components/grigliaProdotti"
+import style from './style.module.scss'
+
+const data = {
+    prodotti: [
+        {
+            name: 'Fermata 125',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/fermata125.png',
+        },
+        {
+            name: 'Ferrero',
+            anno: '2011',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/ferrero.png',
+        },
+        {
+            name: 'Syrah',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/syrah.png',
+        },
+        {
+            name: 'Valpolicella',
+            anno: '1956',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/valpolicella.png',
+        },
+        {
+            name: 'Chianti Classico',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/fermata125.png',
+        },
+        {
+            name: 'Amarone',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/ferrero.png',
+        },
+        {
+            name: 'Merlot',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/syrah.png',
+        },
+        {
+            name: 'Appassimento',
+            anno: '2010',
+            path: '/prodotti/chianti-classico',
+            img: '/immagini-prodotto/valpolicella.png',
+        }
+    ]
+}
+
+
+function Prodotti() {
+    return (
+        <div className={style.container}>
+            <h1>Prodotti</h1>
+            <GrigliaProdotti data={data} />
+        </div>
+    )
+}
+export default Prodotti
