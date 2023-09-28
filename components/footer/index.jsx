b2c599034f78869187f41cc845b36c3b6234515a
grafica <paolo@monema.it>
2023-09-28 16:00:30 +0200

    fix: refactor markup componente

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,12 +2,12 @@ import style from './style.module.scss'
 import Image from 'next/image'
 function Footer() {
     return (
-        <footer>
-            <div className={style.container} >
-                <div className={style.container__boxSx} >
+        <footer className={style.footer}>
+            <div className={style.__container} >
+                <div className={style.__container__boxSx} >
                     <Image src="/ui/logo.svg" alt="logo" width={180} height={40} />
                 </div>
-                <div className={style.container__boxDx} >
+                <div className={style.__container__boxDx} >
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut nulla eveniet veniam, nisi illum, a perspiciatis ipsum dolore modi similique assumenda, quod laboriosam itaque nam odio autem quibusdam obcaecati id.
                 </div>
             </div>
