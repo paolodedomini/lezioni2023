a8724772f29c0a6af679c8d81a196d729c34f3e2
grafica <paolo@monema.it>
2023-09-28 17:38:00 +0200

    feature: logica localstorage

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,18 +1,43 @@
 import style from './style.module.scss'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [login, setLogin] = useState(false)
+    function handleSubmit(email, password) {
+
+
+        if (email && password) {
+            localStorage.setItem('email', email)
+            localStorage.setItem('password', password)
+            localStorage.setItem('login', true)
+        }
+        else { console.log('Please fill in all fields') }
+
+
+    }
+
+
     return (
         <div className={style.login}>
-            <div className={style.login__box}>
+            <motion.div
+                className={style.login__box}
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 100 }}
+                transition={{ duration: 0.5 }}
+            >
                 <div className={style.login__box__formGroup}>
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" />
                 </div>
                 <div className={style.login__box__formGroup}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" />
                 </div>
-                <button>Login</button>
-            </div>
+                <button onClick={() => handleSubmit(email, password)}>Login</button>
+            </motion.div>
         </div>
     )
 }
