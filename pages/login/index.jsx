2116722cbefb146f121351861c64510bfa91c887
grafica <paolo@monema.it>
2023-09-28 16:51:32 +0200

    feature: markup e stile

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -2,13 +2,16 @@ import style from './style.module.scss'
 function Login() {
     return (
         <div className={style.login}>
-            <div className={style.login__formGroup}>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-            </div>
-            <div className={style.login__formGroup}>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
+            <div className={style.login__box}>
+                <div className={style.login__box__formGroup}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className={style.login__box__formGroup}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <button>Login</button>
             </div>
         </div>
     )
