f3639aa9d1705bdd457fecafc700479a99e32ebd
grafica <paolo@monema.it>
2023-09-28 15:57:59 +0200

    fix: refactor markup componente

--- components/grigliaProdotti/index.jsx
+++ components/grigliaProdotti/index.jsx
@@ -19,20 +19,20 @@ function GrigliaProdotti({ data, titolo, num = -1 }) {
     }, [])
 
     return (
-        <div className={style.container}>
+        <div className={style.GrigliaProdotti}>
             <h2>{titolo}</h2>
-            <div className={style.containerLoop} >
+            <div className={style.__containerLoop} >
 
                 {prodotti.map((prodotto, index) => {
                     return (
-                        <div className={style.containerLoop__box} key={index}>
-                            <div className={style.containerLoop__box__img}>
+                        <div className={style.__containerLoop__box} key={index}>
+                            <div className={style.__containerLoop__box__img}>
                                 {prodotto.img ?
                                     <Image src={prodotto.img} alt={prodotto.name} width={80} height={320} /> :
                                     <Image src={'/ui/bottiglia-default.png'} alt={prodotto.name} width={80} height={320} />
                                 }
                             </div>
-                            <div className={style.containerLoop__box__text}>
+                            <div className={style.__containerLoop__box__text}>
                                 <h3>{prodotto.name}
                                     <span>{prodotto.anno}</span>
                                 </h3>
