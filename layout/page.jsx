4e12a7f9404a08b513153089cdad940a130a513e
grafica <paolo@monema.it>
2023-09-28 16:32:07 +0200

    feature: path controllo

--- layout/page.jsx
+++ layout/page.jsx
@@ -1,7 +1,11 @@
 import Navbar from "../components/navbar"
 import { ScrollerMotion } from 'scroller-motion'
 import Footer from "../components/footer"
+import { useRouter } from "next/router"
+
 function LayoutPage({ children }) {
+    const router = useRouter()
+    const { pathname } = router
     return (
         <>
             <ScrollerMotion>
