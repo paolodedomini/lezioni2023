dce4ee6b8a90423922b7e5f9ebe7ba6da43cc887
grafica <paolo@monema.it>
2023-10-02 16:43:34 +0200

    fix: eliminati import non usati

--- layout/page.jsx
+++ layout/page.jsx
@@ -1,11 +1,9 @@
 import Navbar from "../components/navbar"
-import { ScrollerMotion } from 'scroller-motion'
 import Footer from "../components/footer"
-import { useRouter } from "next/router"
+
 
 function LayoutPage({ children }) {
-    const router = useRouter()
-    const { pathname } = router
+
     return (
         <div className="layoutPage" >
 
