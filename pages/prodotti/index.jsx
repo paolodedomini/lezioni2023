f72f67e4400991e082395ce03fac9529b544e9c7
grafica <paolo@monema.it>
2023-10-03 17:07:17 +0200

    feature: prodotti caricati da api

--- pages/prodotti/index.jsx
+++ pages/prodotti/index.jsx
@@ -1,66 +1,19 @@
 import GrigliaProdotti from "../../components/grigliaProdotti"
 import style from './style.module.scss'
-
-const data = {
-    prodotti: [
-        {
-            name: 'Fermata 125',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/fermata125.png',
-        },
-        {
-            name: 'Ferrero',
-            anno: '2011',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/ferrero.png',
-        },
-        {
-            name: 'Syrah',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/syrah.png',
-        },
-        {
-            name: 'Valpolicella',
-            anno: '1956',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/valpolicella.png',
-        },
-        {
-            name: 'Chianti Classico',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/fermata125.png',
-        },
-        {
-            name: 'Amarone',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/ferrero.png',
-        },
-        {
-            name: 'Merlot',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/syrah.png',
-        },
-        {
-            name: 'Appassimento',
-            anno: '2010',
-            path: '/prodotti/chianti-classico',
-            img: '/immagini-prodotto/valpolicella.png',
-        }
-    ]
-}
+import { GET } from "../../utils/HTTPS"
 
 
-function Prodotti() {
+
+function Prodotti({ prodotti }) {
     return (
         <div className={style.prodotti}>
             <h1>Prodotti </h1>
-            <GrigliaProdotti data={data} num={8} />
+            <GrigliaProdotti prodotti={prodotti} num={8} />
         </div>
     )
 }
 export default Prodotti
+export async function getServerSideProps() {
+    const prodotti = await GET('https://api.npoint.io/f2fea3df1468a4daf95f')
+    return { props: { prodotti } }
+}
