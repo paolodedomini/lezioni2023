5bf29e5a2ba2fe3c693acaa0defa11f2b4c482bb
grafica <paolo@monema.it>
2023-10-04 12:57:36 +0200

    feature: page transition con framer

--- layout/page.jsx
+++ layout/page.jsx
@@ -1,19 +1,25 @@
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-
+import { AnimatePresence, motion } from "framer-motion"
+import { useRouter } from "next/router"
 
 function LayoutPage({ children }) {
-
+    const router = useRouter()
     return (
-        <div className="layoutPage" >
+        <AnimatePresence mode="wait" >
+            <motion.div key={router.pathname} className="layoutPage"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                <Navbar />
+                <main>
+                    {children}
+                </main>
+                <Footer />
 
-            <Navbar />
-            <main>
-                {children}
-            </main>
-            <Footer />
-
-        </div>
+            </motion.div>
+        </AnimatePresence>
     )
 
 }
