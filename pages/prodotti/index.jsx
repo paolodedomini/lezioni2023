99066c422efe03c59fe1a6e70e89a59e20712b5e
grafica <paolo@monema.it>
2023-10-05 16:53:35 +0200

    style: stile generale loading

--- pages/prodotti/index.jsx
+++ pages/prodotti/index.jsx
@@ -1,19 +1,22 @@
 import GrigliaProdotti from "../../components/grigliaProdotti"
 import style from './style.module.scss'
 import { GET } from "../../utils/HTTPS"
-
+import Loading from "../../components/loading"
 
 
 function Prodotti({ prodotti }) {
     return (
-        <div className={style.prodotti}>
-            <h1>Prodotti </h1>
-            <GrigliaProdotti prodotti={prodotti} num={8} />
-        </div>
+        <Loading data={prodotti}>
+            <div className={style.prodotti}>
+                <h1>Prodotti </h1>
+                <GrigliaProdotti prodotti={prodotti} num={8} />
+            </div>
+        </Loading>
     )
 }
 export default Prodotti
 export async function getServerSideProps() {
     const prodotti = await GET('https://api.npoint.io/f2fea3df1468a4daf95f')
+
     return { props: { prodotti } }
 }
