1c2e69ecc16b4f7dabad4904a51e58717c40f3a1
grafica <paolo@monema.it>
2023-10-06 10:44:56 +0200

    feature:scroll funzionante da testare

--- layout/page.jsx
+++ layout/page.jsx
@@ -1,19 +1,20 @@
 import Navbar from "../components/navbar"
 import Footer from "../components/footer"
-
+import Scroll from "../components/scroll"
 
 function LayoutPage({ children }) {
 
     return (
+        <Scroll>
+            <div className="layoutPage">
+                <Navbar />
+                <main>
+                    {children}
+                </main>
+                <Footer />
 
-        <div className="layoutPage"           >
-            <Navbar />
-            <main>
-                {children}
-            </main>
-            <Footer />
-
-        </div>
+            </div>
+        </Scroll >
 
     )
 
